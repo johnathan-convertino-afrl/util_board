adb3d093b49ad31884edd93cbc783d84ec362458
Jay Convertino <johnathan.convertino.1@us.af.mil>
2023-11-29 08:25:47 -0500

    wrong options for create ip

--- digilent/cmod-s7-25/cmod-s7-25_system_base.tcl
+++ digilent/cmod-s7-25/cmod-s7-25_system_base.tcl
@@ -3,7 +3,7 @@
 vivado_ip_vlvn_version_check "xilinx.com:ip:clk_wiz:6.0"
 
 # create a pll clock IP with a 100 MHz clock
-create_ip -type ip -vlnv xilinx.com:ip:clk_wiz:6.0 clk_wiz_1
+create_ip -vlnv xilinx.com:ip:clk_wiz:6.0 -module_name clk_wiz_1
 set_property CONFIG.PRIMITIVE MMCM [get_ips clk_wiz_1]
 set_property CONFIG.CLKOUT1_REQUESTED_OUT_FREQ 100 [get_ips clk_wiz_1]
 set_property CONFIG.USE_LOCKED false [get_ips clk_wiz_1]
@@ -17,7 +17,7 @@ set_property generate_synth_checkpoint false [get_files clk_wiz_1.xci]
 vivado_ip_vlvn_version_check "xilinx.com:ip:proc_sys_reset:5.0"
 
 # create a system reset with
-create_ip -type ip -vlnv xilinx.com:ip:proc_sys_reset:5.0 sys_rstgen
+create_ip -vlnv xilinx.com:ip:proc_sys_reset:5.0 -module_name sys_rstgen
 set_property CONFIG.RESET_BOARD_INTERFACE Custom [get_ips sys_rstgen]
 set_property CONFIG.C_EXT_RST_WIDTH 1 [get_ips sys_rstgen]
 set_property CONFIG.C_AUX_RST_WIDTH 1 [get_ips sys_rstgen]
