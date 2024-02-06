fe20b2c0ecc699c87c65df56fa42f87ddc590668
Jay Convertino <johnathan.convertino.1@us.af.mil>
2024-02-06 13:15:08 -0500

    fix for active low reset

--- digilent/nexys-a7-100t/nexys-a7-100t_system_base.tcl
+++ digilent/nexys-a7-100t/nexys-a7-100t_system_base.tcl
@@ -23,8 +23,8 @@ create_ip -vlnv xilinx.com:ip:proc_sys_reset:5.0 -module_name sys_rstgen
 set_property CONFIG.RESET_BOARD_INTERFACE Custom [get_ips sys_rstgen]
 set_property CONFIG.C_EXT_RST_WIDTH 1 [get_ips sys_rstgen]
 set_property CONFIG.C_AUX_RST_WIDTH 1 [get_ips sys_rstgen]
-# set_property CONFIG.C_EXT_RESET_HIGH 1 [get_ips sys_rstgen]
-# set_property CONFIG.C_AUX_RESET_HIGH 0 [get_ips sys_rstgen]
+set_property CONFIG.C_EXT_RESET_HIGH 0 [get_ips sys_rstgen]
+set_property CONFIG.C_AUX_RESET_HIGH 0 [get_ips sys_rstgen]
 
 set_property generate_synth_checkpoint false [get_files sys_rstgen.xci]
 
