34b11ae6177bd8571a1f3f959fd79d435400eb85
Jay Convertino <johnathan.convertino.1@us.af.mil>
2024-06-26 08:12:46 -0400

    add base files for lattice crosslink-nx eval board, templates, not complete

---
+++ lattice/crosslink-nx_eval/crosslink-nx_eval_board_base.tcl
@@ -0,0 +1,32 @@
+# create board base ips
+
+set_msg_config -id "Common 17-55" -new_severity WARNING
+
+ip_vlvn_version_check "xilinx.com:ip:clk_wiz:6.0"
+
+# create a pll clock IP with a 100 MHz clock
+create_ip -vlnv xilinx.com:ip:clk_wiz:6.0 -module_name clk_wiz_1
+set_property CONFIG.PRIMITIVE MMCM [get_ips clk_wiz_1]
+set_property CONFIG.CLKOUT1_REQUESTED_OUT_FREQ 100 [get_ips clk_wiz_1]
+set_property CONFIG.USE_LOCKED false [get_ips clk_wiz_1]
+set_property CONFIG.PRIM_IN_FREQ 12.000 [get_ips clk_wiz_1]
+set_property CONFIG.USE_RESET true [get_ips clk_wiz_1]
+set_property CONFIG.RESET_TYPE {ACTIVE_HIGH} [get_ips clk_wiz_1]
+set_property CONFIG.RESET_PORT {reset} [get_ips clk_wiz_1]
+
+set_property generate_synth_checkpoint false [get_files clk_wiz_1.xci]
+
+ip_vlvn_version_check "xilinx.com:ip:proc_sys_reset:5.0"
+
+# create a system reset with
+create_ip -vlnv xilinx.com:ip:proc_sys_reset:5.0 -module_name sys_rstgen
+set_property CONFIG.RESET_BOARD_INTERFACE Custom [get_ips sys_rstgen]
+set_property CONFIG.C_EXT_RST_WIDTH 1 [get_ips sys_rstgen]
+set_property CONFIG.C_AUX_RST_WIDTH 1 [get_ips sys_rstgen]
+set_property CONFIG.C_EXT_RESET_HIGH 1 [get_ips sys_rstgen]
+set_property CONFIG.C_AUX_RESET_HIGH 0 [get_ips sys_rstgen]
+
+set_property generate_synth_checkpoint false [get_files sys_rstgen.xci]
+
+generate_target all [get_ips]
+
