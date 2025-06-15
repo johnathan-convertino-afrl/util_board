0a32d6653d57d737cb6deb790fc4b16c23d47f52
Jay Convertino <johnathan.convertino.1@us.af.mil>
2025-06-15 10:06:08 -0400

    update crosslink for 50 MHz base default.

--- lattice/crosslink-nx_eval/crosslink-nx_eval_board_base.tcl
+++ lattice/crosslink-nx_eval/crosslink-nx_eval_board_base.tcl
@@ -1,6 +1,6 @@
 # create board base ips
 
-ip_vlvn_version_check "latticesemi.com:module:pll:1.8.0"
+ip_vlvn_version_check "latticesemi.com:module:pll:1.9.0"
 
 prj_add_source "ip/clk_wiz_1/clk_wiz_1.ipx"
 
